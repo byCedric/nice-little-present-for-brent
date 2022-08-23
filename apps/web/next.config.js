7d3157eb084cbae52209b784ff810d86f9425c65
Cedric van Putten <me@bycedric.com>
2022-08-23 13:51:55 +0200

    chore: add workaround for yarn berry

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const withTranspile = require('next-transpile-modules')([
   '@acme/ui',
   'react-native-web',
@@ -15,8 +16,15 @@ module.exports = withTranspile({
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
-      "react-native$": "react-native-web",
+      "react-native$": 'react-native-web', 
     };
+
+    // workaround for Yarn Berry, it tries to resolve react-native-web from @acme/ui
+    config.resolve.modules = [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, '../../packages/ui/node_modules'),
+    ];
+
     config.resolve.extensions = [
       ".web.ts",
       ".web.tsx",
